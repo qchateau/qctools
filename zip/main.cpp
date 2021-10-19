f7c9054481215cd3e787cd4108f0964baf664178
Quentin Chateau <quentin.chateau@gmail.com>
2021-10-19 23:25:41 +0200

    zip: more tests

--- zip/main.cpp
+++ zip/main.cpp
@@ -6,9 +6,24 @@
 
 int main(int, char**)
 {
+    using qctools::zip;
+
+    // lvalue ref
     std::vector<int> v1 = {1, 2, 3};
     std::vector<std::string> v2 = {"a", "b"};
-    for (const auto& pair : qctools::zip(v1, v2)) {
+    for (const auto& pair : zip(v1, v2)) {
+        std::cerr << std::get<0>(pair) << ", " << std::get<1>(pair) << std::endl;
+    }
+
+    // rvalue ref
+    for (const auto& pair : zip(std::vector<int>{1, 2, 3}, std::vector<std::string>{"a", "b"})) {
+        std::cerr << std::get<0>(pair) << ", " << std::get<1>(pair) << std::endl;
+    }
+
+    // const ref
+    const std::vector<int> cv1 = {1, 2, 3};
+    const std::vector<std::string> cv2 = {"a", "b"};
+    for (const auto& pair : zip(cv1, cv2)) {
         std::cerr << std::get<0>(pair) << ", " << std::get<1>(pair) << std::endl;
     }
     return 0;
